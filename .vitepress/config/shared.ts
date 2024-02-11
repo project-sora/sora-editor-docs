f030d46c65432577079221a3393fbff5db18ed19
Rosemoe <2073412493@qq.com>
2024-02-11 18:58:21 +0800

    add favicon

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vitepress'
 export const shared = defineConfig({
   title: 'Sora Editor',
   base: '/sora-editor-docs/',
-
+  head: [['link', { rel: 'icon', href: '/sora-editor-docs/favicon.ico' }]],
   ignoreDeadLinks: true,
   lastUpdated: true,
   cleanUrls: true,
 
   themeConfig: {
+    logo: '/logo.jpg',
     socialLinks: [
       { icon: 'github', link: 'https://github.com/Rosemoe/sora-editor' }
     ],
